541d2d511e1b1ec0a99dfadf74b871347dc44efd
hoang-nguyen13 <97536835+hoang-nguyen13@users.noreply.github.com>
2022-01-11 08:27:25 -0700

    Add files via upload

---
+++ assets/data/discoverData.js
@@ -0,0 +1,24 @@
+const discoverData = [
+    {
+        id: 'discover-1',
+        title: 'pic 1',
+        image: require('../images/img104.jpg')
+    },
+    {
+        id: 'discover-2',
+        title: 'pic 2',
+        image: require('../images/img000.jpg')
+    },
+    {
+        id: 'discover-3',
+        title: 'pic 3',
+        image: require('../images/img001.jpg')
+    },
+    {
+        id: 'discover-4',
+        title: 'pic 4',
+        image: require('../images/img003.jpg')
+    },
+];
+
+export default discoverData;
---
+++ assets/data/homeData.js
@@ -0,0 +1,23 @@
+const homeData = [
+    {
+        id: 'home-1',
+        image: require('../images/home/about-us.png')
+    },
+    {
+        id: 'home-2',
+        title: 'pic 2',
+        image: require('../images/home/learn.png')
+    },
+    {
+        id: 'home-3',
+        title: 'pic 3',
+        image: require('../images/home/news.png')
+    },
+    {
+        id: 'home-4',
+        title: 'pic 4',
+        image: require('../images/home/tips.png')
+    },
+]
+
+export default homeData;
